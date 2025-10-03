1e3c8f0f9f6413f43d2a0f4a9acb7dd40e40e059
abdninesix <mohabd1995@gmail.com>
2025-10-03 23:32:34 +0500

    Updated

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -101,9 +101,9 @@ const Contactpage = () => {
 
   return (
     <div ref={containerRef} className="h-full overflow-hidden">
-      <div className="h-full flex flex-col items-center justify-center gap-8 lg:gap-28 lg:flex-row">
+      <div className="flex flex-col justify-center gap-8 lg:gap-28 lg:flex-row mt-12 lg:mt-28">
 
-        <div className="w-full lg:w-1/2 flex lg:flex-col items-center justify-between">
+        <div className="w-full lg:w-1/2 flex lg:flex-col items-center justify-between lg:justify-center">
           <span ref={textRef} className="text-4xl md:text-6xl font-semibold">Let&apos;s&nbsp;<span>collaborate</span></span>
           <div className="handshake"><FaHandshakeAngle className="size-16 lg:size-72" /></div>
         </div>
