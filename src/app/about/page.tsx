2cc9946e14d188d9fccecd078ce391929fbf23a0
abdninesix <mohabd1995@gmail.com>
2025-10-04 15:22:17 +0500

    Updated

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -63,25 +63,24 @@ const AboutPage = () => {
       );
 
       // 2. Animate each content section as it scrolls into view
-      const sections = gsap.utils.toArray('.scroll-reveal-section');
+      const sections = gsap.utils.toArray<HTMLElement>('.scroll-reveal-section');
       sections.forEach((section) => {
-        const el = section as HTMLElement;
         // Animate the title of the section
-        gsap.from(el.querySelector('.section-title'), {
+        gsap.from(section.querySelector('.section-title'), {
           x: -300,
           opacity: 0,
           duration: 0.8,
           ease: 'power2.out',
           scrollTrigger: {
             scroller: scrollWrapperRef.current, // Specify our scrolling container
-            trigger: el,
+            trigger: section,
             start: "top 60%",
             toggleActions: "play none none none",
           }
         });
 
         // Animate the content of the section
-        gsap.from(el.querySelector('.section-content'), {
+        gsap.from(section.querySelector('.section-content'), {
           x: -300,
           opacity: 0,
           duration: 0.8,
@@ -89,7 +88,7 @@ const AboutPage = () => {
           ease: 'power2.out',
           scrollTrigger: {
             scroller: scrollWrapperRef.current,
-            trigger: el,
+            trigger: section,
             start: "top 60%",
             toggleActions: "play none none none",
           }
