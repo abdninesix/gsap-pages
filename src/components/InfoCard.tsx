062e2673b7009efe88cc569a5c9ba0bf12b86d72
abdninesix <mohabd1995@gmail.com>
2025-10-04 14:31:34 +0500

    Updated

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -7,7 +7,7 @@ interface InfoCardProps {
 const InfoCard = ({ title, description, date }: InfoCardProps) => {
     return (
         <div className="w-5/12 relative bg-mytheme/20 p-4 duration-200">
-            <div className="shadow-lg p-2 md:p-4 bg-white text-gray-800 font-semibold rounded-md text-center w-fit">
+            <div className="shadow-lg p-2 md:p-4 bg-white text-black font-semibold rounded-md text-center w-fit">
                 {title}
             </div>
             <div className="-mx-3 p-3 text-[12px] md:text-sm">
